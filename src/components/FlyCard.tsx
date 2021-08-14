67507dcf3b3b7a4ca75e640d1c9439b9a04ee7fe
gramm2009 <viktorkaturkin@gmail.com>
2021-08-14 23:15:53 +0300

    update

--- src/components/FlyCard.tsx
+++ src/components/FlyCard.tsx
@@ -1,8 +1,8 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { FlightType } from '../mobX/State';
-import { time } from '../../utils/helpers/time';
-import { duration } from '../../utils/helpers/duration';
+import { time } from '../../utils/helpers/time.ts';
+import { duration } from '../../utils/helpers/duration.ts';
 
 type FlyCardPropsType = {
     el: FlightType;
