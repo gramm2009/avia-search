3a76f896df37098be88ec1c0a8389838777b2c4c
gramm2009 <viktorkaturkin@gmail.com>
2021-08-15 12:49:46 +0300

    updating the architecture

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import '../styles/__all.scss';
+import './styles/__all.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
